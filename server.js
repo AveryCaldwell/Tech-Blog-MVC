b61ee3ff7f685439e1823740d05f64f1067f5fb1
Avery <averycaldwell7@gmail.com>
2023-02-20 18:20:12 -0800

    edit requirements

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const session = require('express-session');
-const routes = require('./controllers');
+// const routes = require('./controllers');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 const path = require('path');
