a20d02836a97795ca6225d81954d6b5db0c49af8
Avery <averycaldwell7@gmail.com>
2023-02-23 13:38:24 -0800

    corrected helper date

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,10 +1,9 @@
 module.exports = {
+
+
     format_date: (date) => {
-        // Format date as MM/DD/YYYY
-        return date.toLocaleDateString();
-    },
-    format_amount: (amount) => {
-        // format large numbers with commas
-        return parseInt(amount).toLocaleString();
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
+            new Date(date).getFullYear() + 5
+        }`;
     },
 };
