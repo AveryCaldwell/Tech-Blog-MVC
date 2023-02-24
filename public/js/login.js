22d4654fc57d4cc4b38c1cca9f637826d6e4ed73
Avery <averycaldwell7@gmail.com>
2023-02-24 13:01:58 -0800

    fixed login url

--- public/js/login.js
+++ public/js/login.js
@@ -6,7 +6,7 @@ const loginFormHandler = async (event) => {
     const password = document.querySelector('#password-login').value.trim();
 
     if (username && email && password) {
-        const response = await fetch('/users/login', {
+        const response = await fetch('api/users/login', {
             method: 'POST',
             body: JSON.stringify({ username, email, password }),
             headers: { 'Content-Type': 'application/json' },
